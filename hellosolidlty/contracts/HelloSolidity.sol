18688fd87b66429a3771da2f2f0f6d4965bd1737
kanut karomprach <kanut@sec.or.th>
2020-07-23 14:26:28 +0700

    update 2

--- hellosolidlty/contracts/HelloSolidity.sol
+++ hellosolidlty/contracts/HelloSolidity.sol
@@ -10,8 +10,34 @@
 */
 
 pragma solidity >=0.5.0;
-unit public myValue;
 
 contract HelloSolidity {
+
+    // state variable
+    uint public myValue;
+
+    constructor() public {
+        myValue =  5;
+    }
+
+    function increase() public  { // transactional call => need transaction
+        myValue++;
+    }
+
+    function setValue(uint newValue) public {
+        myValue = newValue;
+    }
+
+    function getValue() public returns (uint) { // non-transactional call
+        return myValue;
+    }
+
+    function decrease() public {
+        myValue--;
+    }
+
+    function myFunc(uint a, uint b) public returns (uint, uint) {
+        return (a*b, a+b);
+    }
 
 }
