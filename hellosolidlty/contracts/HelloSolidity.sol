682d625bdd29a27fdf057acd03b49d44813918c0
kanut karomprach <kanut@sec.or.th>
2020-07-23 10:49:58 +0700

    initial

--- hellosolidlty/contracts/HelloSolidity.sol
+++ hellosolidlty/contracts/HelloSolidity.sol
@@ -1,1 +1,17 @@
-pragma solidity >= 0.5.0;
+/*
+    Contract-oriented
+        class = contract
+        method = function
+        instnce variavle = state vairables
+
+        Value type = primitive type (int / bool)
+        Reference type = complex type (array / structure)
+        mapping = dictionary
+*/
+
+pragma solidity >=0.5.0;
+unit public myValue;
+
+contract HelloSolidity {
+
+}
